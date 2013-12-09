55007808d516b5fd6eeaf5e7ec6b3350a1def72b
NurAtmaHidayat <atma_4771@yahoo.com>
2013-12-09 22:28:30 +0700

    Bootstrap
    Bootstrap

---
+++ Bootstrap/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Menu Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <!--<a class="navbar-brand" href="#">Brand</a> -->
+  </div>
+    <ul class="nav navbar-nav navbar-right"> 
+  
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+         
+		 <form class="navbar-form navbar-left" role="Email">
+		 <form class="navbar-form navbar-left" role="Password">
+	  <div class="form-group">
+        <input type="text" class="form-control" placeholder="Email">
+      </div>
+	  
+	   <div class="form-group">
+        <input type="text" class="form-control" placeholder="Masukkan Password">
+      </div>
+	  
+      <button type="submit" class="btn btn-default">Login</button>
+    </form>
+        </ul>
+      </li>
+	  
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
+  <h1>Hellow!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+  
+</html>
